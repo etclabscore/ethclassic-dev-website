de957d4f84355f01e593068843638c43106ac578
Stevan Lohja <stevanio@protonmail.com>
2019-07-29 21:09:48 -0500

    fix: update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,17 +11,4 @@ module.exports = {
       { text: '⚠️ Atlantis', link: '/news/' },
     ]
   },
-  head: [
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,maximum-scale=1' }],
-    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
-    ['meta', { property: 'og:image', content: 'https://github.com/open-rpc/design/raw/master/png/open-rpc-logo-320x320.png?raw=true' }],
-    ['script', { async: true, defer: true, src: 'https://cdn.jsdelivr.net/npm/@widgetbot/crate@3' },
-    `
-      new Crate({
-        server: '538420083614023710',
-        channel: '543112562405933116',
-        shard: 'https://disweb.deploys.io'
-      })
-    `]
-  ]
 }
